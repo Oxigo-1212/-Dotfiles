a619e9fea544f9df217e6c7cbd51a1fe11c60fa9
VietHung-1212 <24021502@vnu.edu.vn>
2026-01-20 13:07:00 +0700

    Fix lua_ls undefined vim

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,37 @@
 return {
 	{
 		"mason-org/mason-lspconfig.nvim",
-		opts = {
-			server = {
-				copilot = { enabled = true },
-			},
-		},
 		dependencies = {
 			{ "mason-org/mason.nvim", opts = {} },
 			"neovim/nvim-lspconfig",
 		},
+		opts = {
+			servers = {
+				copilot = { enabled = true },
+				lua_ls = {
+					settings = {
+						Lua = {
+							diagnostics = {
+								globals = { "vim" },
+							},
+							workspace = {
+								library = vim.api.nvim_get_runtime_file("", true),
+								checkThirdParty = false,
+							},
+							telemetry = {
+								enable = false,
+							},
+						},
+					},
+				}
+			}
+		},
+		config = function(_, opts)
+			for server, config in pairs(opts.servers) do
+				vim.lsp.config(server, config)
+				vim.lsp.enable(server)
+			end
+		end
 	},
 	{ 'WhoIsSethDaniel/mason-tool-installer.nvim' },
 	{
