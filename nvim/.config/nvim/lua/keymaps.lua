4f8ccf7fa46f5f8aa7cabc319895c91b4ed95103
VietHung-1212 <24021502@vnu.edu.vn>
2026-02-04 14:50:47 +0700

    Add ghostty to my dotfiles

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -44,8 +44,8 @@ vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Find recently open
 vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+-- vim.keymap.set("n", "<C-d>", "<C-d>zz")
+-- vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
