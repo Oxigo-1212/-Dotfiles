ca3b76fcd6351ab13cde26dff7d23c0869cf31ee
VietHung-1212 <24021502@vnu.edu.vn>
2026-08-02 08:03:15 +0700

    update the status bar to follow the noctaria colorscheme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -65,9 +65,6 @@ function ColorMyPencils(color)
 end
 
 vim.o.statusline = "%{&fileformat}[%{&filetype}] %F%=%l/%L %l:%c %P"
-vim.api.nvim_set_hl(0, "StatusLine", { fg = "#C5C9C7", bg = "NONE" })
-vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "#A4A7A4", bg = "NONE" })
-vim.cmd(":hi statusline guibg=NONE")
 -- ColorMyPencils("catppuccin-nvim")
 -- vim.cmd.colorscheme("solarized-osaka")
 vim.api.nvim_set_hl(0, "Cursor", {
@@ -95,74 +92,68 @@ require("layout")
 -- 	return root and vim.fs.basename(root) or vim.fs.basename(vim.fn.getcwd())
 -- end
 
--- require("lualine").setup({
--- 	options = {
--- 		icons_enabled = false,
--- 		theme = "auto",
--- 		disabled_filetypes = {
--- 			statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" },
--- 			winbar = {},
--- 		},
--- 		ignore_focus = {},
--- 		always_divide_middle = true,
--- 		always_show_tabline = true,
--- 		globalstatus = true,
--- 		refresh = {
--- 			statusline = 1000,
--- 			tabline = 1000,
--- 			winbar = 1000,
--- 			refresh_time = 16, -- ~60fps
--- 			events = {
--- 				"WinEnter",
--- 				"BufEnter",
--- 				"BufWritePost",
--- 				"SessionLoadPost",
--- 				"FileChangedShellPost",
--- 				"VimResized",
--- 				"Filetype",
--- 				"CursorMoved",
--- 				"CursorMovedI",
--- 				"ModeChanged",
--- 			},
--- 		},
--- 	},
--- 	sections = {
--- 		lualine_a = { "mode" },
--- 		lualine_b = { "branch" },
--- 		lualine_c = {
--- 			project_root,
--- 			"diagnostics",
--- 			{ "filename", path = 1 },
--- 		},
--- 		lualine_x = {
--- 			"diff",
--- 			"encoding",
--- 			"fileformat",
--- 			"filetype",
--- 		},
--- 		lualine_y = {
--- 			{ "progress", separator = " ",                  padding = { left = 1, right = 0 } },
--- 			{ "location", padding = { left = 0, right = 1 } },
--- 		},
--- 		lualine_z = {
--- 			function()
--- 				return os.date("%R")
--- 			end,
--- 		},
--- 	},
--- 	inactive_sections = {
--- 		lualine_a = {},
--- 		lualine_b = {},
--- 		lualine_c = { { "filename", path = 1 } },
--- 		lualine_x = { "location" },
--- 		lualine_y = {},
--- 		lualine_z = {},
--- 	},
--- 	tabline = {},
--- 	winbar = {},
--- 	inactive_winbar = {},
--- 	extensions = { "lazy" },
--- })
+require("lualine").setup({
+	options = {
+		icons_enabled = false,
+		theme = "auto",
+		component_separators = { left = "|", right = "|" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = {
+			statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" },
+			winbar = {},
+		},
+		ignore_focus = {},
+		always_divide_middle = true,
+		always_show_tabline = true,
+		globalstatus = true,
+		refresh = {
+			statusline = 1000,
+			tabline = 1000,
+			winbar = 1000,
+			refresh_time = 16, -- ~60fps
+			events = {
+				"WinEnter",
+				"BufEnter",
+				"BufWritePost",
+				"SessionLoadPost",
+				"FileChangedShellPost",
+				"VimResized",
+				"Filetype",
+				"CursorMoved",
+				"CursorMovedI",
+				"ModeChanged",
+			},
+		},
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch" },
+		lualine_c = {
+			{ "filename", path = 1 },
+		},
+		lualine_x = {
+			"fileformat",
+			"encoding",
+			"filetype",
+		},
+		lualine_y = {
+			"progress",
+		},
+		lualine_z = { "location" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_c = { { "filename", path = 1 } },
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	winbar = {},
+	inactive_winbar = {},
+	extensions = { "lazy" },
+})
 require("mason").setup()
 require("mini.surround").setup()
 require("mini.pairs").setup()
